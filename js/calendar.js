8f406dfe01b57fe9e9152b4bce895a835d716d56
Andreas Lundkvist <github@galdiuz.com>
2016-02-02 11:58:27 +0100

    Added weeknumbers to calendar

--- js/calendar.js
+++ js/calendar.js
@@ -15,10 +15,10 @@
         if(now.toDateString() == date.toDateString()) return;
 
         var html = "<table>";
-        html += "<tr>";
+        html += "<tr><td class='week'></td>";
         html += "<td colspan='7' class='month'>" + months[now.getMonth()] + " " + now.getFullYear() + "</td>";
         html += "</tr>";
-        html += "<tr>";
+        html += "<tr><td class='week'></td>";
         html += "<th>Må</th>"
         html += "<th>Ti</th>"
         html += "<th>On</th>"
@@ -35,6 +35,7 @@
 
         for(var i = 0; i < 6; i++) {
             html += "<tr>";
+			html += "<td class='week'>" + (day.getWeek() + 1) + "</td>";
             for(var j = 0; j < 7; j++) {
                 day.setDate(day.getDate() + 1);
                 if(day.getMonth() == now.getMonth()) {
@@ -60,4 +61,13 @@
                 (59 - now.getSeconds()) * 1000;
         setTimeout(update, next);
     }
+
+    Date.prototype.getWeek = function() {
+        var date = new Date(this.getTime());
+        date.setHours(0, 0, 0, 0);
+        date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+        var week1 = new Date(date.getFullYear(), 0, 4);
+        return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                - 3 + (week1.getDay() + 6) % 7) / 7);
+}
 })();
