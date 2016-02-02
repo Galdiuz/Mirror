d8d2e462bb6000903712a68edaf7bdf9048173f2
Andreas Lundkvist <github@galdiuz.com>
2016-02-02 10:40:32 +0100

    Added alert, changed image syntax

--- js/file.js
+++ js/file.js
@@ -3,8 +3,8 @@
 
     function readFile() {
         $.post("php/readFile.php", function(text) {
-            text = text.replace(/&lt;img src=&quot;/g, "<img src=\"");
-            text = text.replace(/&quot;&gt;&lt;\/img&gt;/g, "\"></img>");
+            text = text.replace(/&lt;img&gt;/g, "<img src=\"");
+            text = text.replace(/&lt;\/img&gt;/g, "\"></img>");
             $("#file").html(text);
         });
         setTimeout(readFile, 15000);
--- set.php
+++ set.php
@@ -1,20 +1,23 @@
 <html>
 <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <title>Mirror - Set Text</title>
-    <meta name="google" value="notranslate" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <meta name="google" value="notranslate">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <textarea id="textbox" type="text"><?php include("php/readFile.php"); ?></textarea>
-    <br />
-    <input type="button" value="Save" onclick="writeFile()">
+    <br>
+    <input type="button" value="Spara" onclick="writeFile()">
+    <p>Bilder:<br>&lt;img&gt;http://www.example.com/image.jpg&lt;/img&gt;</p>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script>
     function writeFile() {
         var text = $("#textbox").val();
-        $.post("php/writeFile.php", { text: text });
+        $.post("php/writeFile.php", { text: text }, function() {
+            alert("Ändringar sparade");
+        });
     }
     </script>
 </body>
